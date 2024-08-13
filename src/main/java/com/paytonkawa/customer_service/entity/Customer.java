ee0478fdd1ec984d98ba3e4ca43e6f4b23ffc6ba
achahbar <shadys.50c@gmail.com>
2024-08-13 15:48:55 +0200

    minor changes to customer class

--- src/main/java/com/paytonkawa/customer_service/entity/Customer.java
+++ src/main/java/com/paytonkawa/customer_service/entity/Customer.java
@@ -27,4 +27,69 @@ public class Customer {
 	private String email;
 	@OneToOne(mappedBy = "customer")
 	private Adress adress;
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(
+			@NotBlank(message = "firstname needed for customer") @Size(min = 2, message = "firstname should be at least 2 characters long") String fistname,
+			@NotBlank(message = "lastname needed for customer") @Size(min = 2, message = "lastname should be at least 2 characters long") String lastname,
+			@NotBlank(message = "email cannot be empty") @Email(message = "not a valid email adress ") String email,
+			Adress adress) {
+		super();
+		this.fistname = fistname;
+		this.lastname = lastname;
+		this.email = email;
+		this.adress = adress;
+	}
+
+	public String getFistname() {
+		return fistname;
+	}
+
+	public void setFistname(String fistname) {
+		this.fistname = fistname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Adress getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Adress adress) {
+		this.adress = adress;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fistname=" + fistname + ", lastname=" + lastname + ", email=" + email
+				+ ", adress=" + adress + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
